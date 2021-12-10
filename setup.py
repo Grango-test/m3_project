1e30a1b9fa603ec383c82c7ff0593a8059e443ba
vladislav <grango@list.ru>
2021-12-10 09:05:27 +0300

    setup script

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup
 
 setup(
-    name='m3_project',
-    version='',
-    packages=[''],
+    name='m3-project',
+    version='1.0.0',
+    packages=['m3_project', 'm3_project.m3_project', 'm3_project.app', 'm3_project.app.migrations'],
     url='',
     license='',
     author='vladislav',
     author_email='',
-    description=''
+    description='',
+    install_requires=[
+            'django==2.2',
+            'm3-django-compat==1.9.2',
+            'm3-objectpack==2.2.47']
 )
